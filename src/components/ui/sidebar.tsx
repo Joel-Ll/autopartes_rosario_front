038708b39d7860439c7f2b3c2655d7c1313d4344
Joel-Ll <joellanos654@gmail.com>
2026-07-30 18:59:42 -0400

    hide sidebar on mobil

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -88,6 +88,12 @@ function SidebarProvider({
     [setOpenProp, open]
   )
 
+  React.useEffect(() => {
+    if (isMobile) {
+      setOpenMobile(false)
+    }
+  }, [location.pathname, isMobile])
+
   // Helper to toggle the sidebar.
   const toggleSidebar = React.useCallback(() => {
     return isMobile ? setOpenMobile((open) => !open) : setOpen((open) => !open)
